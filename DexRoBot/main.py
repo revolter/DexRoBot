revolter/DexRoBot

commit 30a913cccfc35f4d1b23e29537a04294c5b1623e

Replaced relative index with absolute index for definitions

DexRoBot/main.py
@@ -97,6 +97,10 @@ def inline_query_handler(bot, update):
 
         dexUrl = dexAPIUrl[:- 5] # /json
 
+    # set the index of the definitions
+    for index in range(len(dexRawDefinitions)):
+        dexRawDefinitions[index]['index'] = index
+
     if args.index is not None:
         if args.index >= len(dexRawDefinitions):
             logger.warn('Index out of bounds')
@@ -108,19 +112,19 @@ def inline_query_handler(bot, update):
     results = list()
 
     for dexRawDefinition in dexRawDefinitions:
+        dexDefinitionIndex = dexRawDefinition['index']
+
         dexDefinitionId = dexRawDefinition['id']
         dexDefinitionSourceName = dexRawDefinition['sourceName']
         dexDefinitionAuthor = dexRawDefinition['userNick']
         dexDefinitionInternalRep = dexRawDefinition['internalRep']
-
-        index = dexRawDefinitions.index(dexRawDefinition)
 
         dexDefinitionTitle = dexDefinitionInternalRep
 
         dexDefinitionTitle = ALL_SIGNS_REGEX.sub('', dexDefinitionTitle)
 
         if args.debug:
-            dexDefinitionTitle = '{}: {}'.format(index, dexDefinitionTitle)
+            dexDefinitionTitle = '{}: {}'.format(dexDefinitionIndex, dexDefinitionTitle)
 
         dexDefinitionTitle = dexDefinitionTitle[:MESSAGE_TITLE_LENGTH_LIMIT + 1]
 

commit b9c15f30d8a520277b44be44ffc0cb54263ddf46

Fixed incorrect debug log definition index
(introduced by 30a913cc)

DexRoBot/main.py
@@ -249,7 +249,7 @@ def inline_query_handler(bot, update):
         dexDefinitionHTMLRep = '{}\n{}'.format(dexDefinitionHTMLRep, dexDefinitionFooter)
 
         if args.debug:
-            logger.info('Result: {}: {}'.format(index, dexDefinitionHTMLRep))
+            logger.info('Result: {}: {}'.format(dexDefinitionIndex, dexDefinitionHTMLRep))
 
         dexDefinitionResult = InlineQueryResultArticle(
             id=uuid4(),