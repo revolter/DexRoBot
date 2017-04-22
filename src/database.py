30657eb20612817695a658fd6a7626b2bb8a7955
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-04-22 12:20:12 +0300

    Added created and updated timestamps to the User database model

--- src/database.py
+++ src/database.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from uuid import uuid4
 
 import logging
 
 from peewee import (
     Model,
-    TextField, BigIntegerField,
+    DateTimeField, TextField, BigIntegerField,
     PeeweeException, DoesNotExist
 )
 from playhouse.sqlite_ext import SqliteExtDatabase
@@ -18,12 +19,15 @@ database = SqliteExtDatabase('dex.sqlite')
 
 
 class BaseModel(Model):
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField()
+
     class Meta:
         database = database
 
 
 class User(BaseModel):
-    id = TextField(primary_key=True, unique=True, default=uuid4())
+    id = TextField(primary_key=True, unique=True, default=uuid4)
     telegram_id = BigIntegerField()
     telegram_username = TextField()
 
@@ -32,13 +36,16 @@ database.connect()
 User.create_table(True)
 
 def create_user(id, username):
+    current_date_time = datetime.now()
+
     try:
         try:
             db_user = User.get(User.telegram_id == id)
 
             db_user.telegram_username = username
+            db_user.updated_at = current_date_time
         except (PeeweeException, DoesNotExist):
-            db_user = User.create(telegram_id=id, telegram_username=username)
+            db_user = User.create(telegram_id=id, telegram_username=username, updated_at=current_date_time)
 
         if db_user:
             db_user.save()
