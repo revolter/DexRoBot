bfb3622567fa90c5c2d68159d2cd892c8e09d2e4
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-04-25 22:47:10 +0300

    Fixed admin check error message

--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,7 @@ def check_admin(bot, message, analytics, admin_user_id):
     analytics.track(AnalyticsType.COMMAND, message.from_user, message.text)
 
     if not admin_user_id or message.from_user.id != admin_user_id:
-        bot.sendMessage(message.chat_id, 'You are not allowed to restart the bot')
+        bot.sendMessage(message.chat_id, 'You are not allowed to use this command')
 
         return False
 
