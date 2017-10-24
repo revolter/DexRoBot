d974243c5bc36eba1b2f816ab9128c79bcdf7bd5
Iulian Onofrei <iulian.onofrei@yahoo.com>
2017-10-24 22:54:32 +0300

    Set deploy as the default Fabric task

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import cd, env, put, run
+from fabric.api import cd, env, put, run, task
 
 import configparser
 
@@ -34,6 +34,7 @@ env.colorize_errors = True
 env.warn_only = True
 
 
+@task
 def setup():
     run('mkdir -p {0.project_path}/{0.project_name}'.format(env))
     run('ln -s {0.project_path}/{0.project_name} ~/{0.project_name}'.format(env))
@@ -42,11 +43,13 @@ def setup():
         run('virtualenv -p python3 env')
 
 
+@task
 def cleanup():
     run('rm -r ~/{.project_name}'.format(env))
     run('rm -r {0.project_path}/{0.project_name}'.format(env))
 
 
+@task(default=True)
 def deploy(filename=None):
     with cd('~/{.project_name}'.format(env)):
         if not filename:
@@ -58,6 +61,7 @@ def deploy(filename=None):
             put('src/{}'.format(filename), '~/{.project_name}/'.format(env))
 
 
+@task
 def execute(command=None):
     if not command:
         return
